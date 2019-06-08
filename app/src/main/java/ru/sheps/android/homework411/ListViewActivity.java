52e0e4392a5362a52fd325317882d97920f1bf18
Katya Sheps <katya@sheps.ru>
2019-06-09 01:23:09 +0300

    Start

--- app/src/main/java/ru/sheps/android/homework411/ListViewActivity.java
+++ app/src/main/java/ru/sheps/android/homework411/ListViewActivity.java
@@ -1,11 +1,17 @@
 package ru.sheps.android.homework411;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class ListViewActivity extends AppCompatActivity {
@@ -14,24 +20,36 @@ public class ListViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
         ListView list = findViewById(R.id.list);
 
-        String[] values = prepareContent();
+        List<Map<String, String>> values = prepareContent();
 
-        BaseAdapter listContentAdapter = createAdapter(values);
+        String[] from = {"Header", "Subheader"};
+        int[] to = {R.id.header, R.id.subheader};
+        BaseAdapter listContentAdapter = createAdapter(values, from, to);
 
         list.setAdapter(listContentAdapter);
     }
 
-
-    private BaseAdapter createAdapter(String[] values) {
-        return new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, values);
+    private BaseAdapter createAdapter(List<Map<String, String>> values, String[] from, int[] to) {
+        return new SimpleAdapter(this, values, R.layout.my_simple_list_item, from, to);
     }
 
-    private String[] prepareContent() {
-        return getString(R.string.large_text).split("\n\n");
+    private List<Map<String, String>> prepareContent() {
+    String[] strings = getString(R.string.large_text).split("\n\n");
+
+        List<Map<String, String>> list = new ArrayList<>();
+
+        for (int i = 0; i < strings.length; i++) {
+            Map<String, String> map = new HashMap<String, String>();
+            map.put("Header", strings[i]);
+            map.put("Subheader",  String.valueOf(strings[i].length()));
+            list.add(map);
+        }
+
+        return list;
     }
 }
